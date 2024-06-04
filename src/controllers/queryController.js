a8f8f379ae69b2616630efaaf063203bb02b7a1d
Deiby Gomez <gokaju23@gmail.com>
2024-06-03 22:37:59 -0500

    Refactor queryController to handle and return image data

--- src/controllers/queryController.js
+++ src/controllers/queryController.js
@@ -7,17 +7,24 @@ const getQueryResult = async (req, res) => {
       return res.status(400).json({ error: "annotationId is required" });
     }
 
-    const query = `SELECT e.empl_nombres, e.empl_firma 
+    const query = `SELECT e.empl_nombres, e.empl_firma, e2.empl_firma as 'firma_aux'
                     FROM javaphc.anotaciones a
                     INNER JOIN javap.usuarios u ON a.anot_registradopor = u.usua_id
                     LEFT  JOIN javap.empleados e ON u.usua_idpersona = e.empl_id
-                    WHERE anot_id =${annotationId}`;
+                    left join javaphc.anotaciones_sello as2 on a.anot_id = as2.anot_id 
+                    left JOIN javap.usuarios u2 ON as2.anot_registradopor  = u2.usua_id
+                    LEFT  JOIN javap.empleados e2 ON u2.usua_idpersona = e2.empl_id
+                    WHERE a.anot_id = ${annotationId}`;
+
     const result = await executeQuery(query);
 
-    if (result.length === 0 || !result[0].empl_firma) {
+    if (
+      result.length === 0 ||
+      (!result[0].empl_firma && !result[0].firma_aux)
+    ) {
       return res.status(404).json({ error: "No results found" });
     }
-    const imageBase64 = result[0].empl_firma;
+    const imageBase64 = result[0]?.firma_aux ?? result[0].empl_firma;
     const imageBuffer = Buffer.from(
       imageBase64.replace("data:image/png;base64,", ""),
       "base64"
